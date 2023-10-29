3d15041ee817d43fcccecfe5bf85a7127021c58b
AayushBhusari <bhusariaayush29@gmail.com>
2023-10-29 11:09:55 +0530

    first commit

---
+++ questions.js
@@ -0,0 +1,19 @@
+let ques = [{
+    question:"What will be the path of electron when it is moving in transverse magnetic field?",
+    opt1:"Straight Line",
+    opt2:"Parabolic",
+    opt3:"Helical",
+    opt4:"Circular",
+    answer:"Circular",
+    chap:"electron-ballitics"
+  },
+  {
+    question:"If an electron enters a transverse Magnetic Field at an angle, What would be the shape of it's path?",
+    opt1:"Straight Line",
+    opt2:"Parabolic",
+    opt3:"Helical",
+    opt4:"Circular",
+    answer:"Circular",
+    chap:"electron-ballitics"
+  }
+  ]
---
+++ script.js
@@ -0,0 +1,9 @@
+//JS FOR NAVBAR START
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+//JS FOR NAVBAR END
+
